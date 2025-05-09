17ef65468935850cec118ea8b9fb1d4bb9918417
nrhdyt3012 <dayats354313@gmail.com>
2025-05-09 10:13:56 +0700

    create a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { LoadingScreen } from './components/LoadingScreen';
 import "./index.css";
 import { useState } from "react";
 import { useEffect } from "react";
+import { Navbar } from './components/Navbar';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
@@ -12,7 +13,7 @@ function App() {
 {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />} 
    <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black  text-gray-100`}
    >
-
+    <Navbar />
    </div>
    </>
   );
