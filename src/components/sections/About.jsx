82c237c632f96dfb0bfb670a28ec0d5337744886
nrhdyt3012 <dayats354313@gmail.com>
2025-05-15 13:48:31 +0700

    add about section skills

---
+++ src/components/sections/About.jsx
@@ -0,0 +1,50 @@
+export const About = () => {
+
+    const frontendSkills = ["React", "TypeScript", "TailwindCSS", "Svelte"];
+    const backendSkills = ["NodeJS","Python", "MongoDB", "PostgreeSQL", "Svelte"];
+
+    return (
+        <section id="about" className="min-h-screen flex items-center justify-center py-20">
+            <div className="max-w-3xl mx-auto px-4">
+                <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+                    {""}
+                    About Me
+                    </h2>
+                <div className="glass rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+                    <p className="text-gray-300 text-lg mb-6">
+                        💡 I have a strong interest in Mobile Development, Backend, Data Analysis, and Frontend Development.
+                        I have studied Android application development using Kotlin.
+                        In the data field, I am experienced in data exploration, visualization, and processing using Python, R, and Excel.
+                        For frontend development, I am proficient in HTML, CSS, and JavaScript, as well as frameworks like ReactJS and Vue.js. I also have experience with PHP and Laravel in building responsive and dynamic user interfaces.
+                    </p>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4">FrontEnd</h3>
+                            <div className="flex flex-wrap gap-2 ">
+                                {frontendSkills.map((tech, key) => (
+                                    <span key={tech} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                                    ">
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                         <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4">FrontEnd</h3>
+                            <div className="flex flex-wrap gap-2 ">
+                                {backendSkills.map((tech, key) => (
+                                    <span key={tech} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                                    ">
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -2,7 +2,7 @@ export const Home = () => {
     return (
     <section id="home" className="min-h-screen flex items-center justify-center relative">
     <div className="text-center z-10 px-4">
-    <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent  leading-right mb-8">Hi, I am Dwi Nurhidayat</h1>
+    <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent  leading-right mb-8">Hi, I am Dwi Nurhidayat</h1>
     <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
 💡 I have a strong interest in Mobile Development, Backend, Data Analysis, and Frontend Development.
      I have studied Android application development using Kotlin. 
