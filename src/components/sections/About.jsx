71090c325a9bd046881aab3a0e8ff8ab8a2b7cd6
nrhdyt3012 <dayats354313@gmail.com>
2025-05-16 11:37:23 +0700

    Mempercantik efek ketika tampil

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,4 @@
+import { RevealOnScroll } from "../RevealOnScroll";
 export const About = () => {
 
     const frontendSkills = ["React", "TypeScript", "TailwindCSS", "Svelte"];
@@ -5,6 +6,8 @@ export const About = () => {
 
     return (
         <section id="about" className="min-h-screen flex items-center justify-center py-20">
+                    <RevealOnScroll>
+            
             <div className="max-w-3xl mx-auto px-4">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
                     {""}
@@ -76,6 +79,8 @@ export const About = () => {
                     
                 </div>
             </div>
+                                </RevealOnScroll>
+
         </section>
     )
 }
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,5 +1,9 @@
+import { RevealOnScroll } from "../RevealOnScroll"
+
 export const Projects = () => {
     return <section id="projects" className="min-h-screen flex items-center justify-center py-20">
+                            <RevealOnScroll>
+        
         <div className="max-w-5xl mx-auto px-4">     
         <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
                 {""}
@@ -109,5 +113,7 @@ Aplikasi yang dibuat untuk memenuhi tugas dicoding terkait belajar penerapan mac
             </div>
         </div>
         </div>
+                                    </RevealOnScroll>
+
     </section>
 }
